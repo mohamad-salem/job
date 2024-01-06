db8531cf88d1d73b5ea70fb5fa2f719922d41f9f
mohamad-salem <92520664+mohamad-salem@users.noreply.github.com>
2024-01-06 17:39:24 +0400

    Update db.js

--- job-board/server/database/db.js
+++ job-board/server/database/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const DBConnection = async () => {
 
-    const MONGO_URI = `mongodb+srv://user:codeforinterview@job-portal.ri6pbum.mongodb.net/`;
+       const MONGO_URI = `mongodb+srv://user:Asd123456789@user-authentication.nxutecf.mongodb.net/`;
     try {
         await mongoose.connect(MONGO_URI, { useNewUrlParser: true });
         console.log('Database connected successfully');
